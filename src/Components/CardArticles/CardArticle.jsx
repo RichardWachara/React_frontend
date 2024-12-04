a614ea3c6951f12b0fee2bd8dfd7d9703b61716d
RichardWachara <richardwachara99@gmail.com>
2024-12-04 09:58:05 +0300

    Corrected the card image url

--- src/Components/CardArticles/CardArticle.jsx
+++ src/Components/CardArticles/CardArticle.jsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 import "./CardArticle.css";
 
 const CardArticle = (props) => {
-
-  const BASE_URL = import.meta.env.VITE_BASE_URL
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
 
   return (
-    <div className="article-container" style={{
-      border: props.border
-    }}>
+    <div
+      className="article-container"
+      style={{
+        border: props.border,
+      }}
+    >
       <Link to={`/product/?id=${props.product.id}`}>
         <article className="article-card">
           <figure className="article-image">
-            <img src={`${BASE_URL}/${props.product.image}`} alt="Watch image" />
+            <img src={`${BASE_URL}${props.product.image}`} alt="Watch image" />
           </figure>
           <div className="article-content">
             <span className="card-category">REF 126710BLRO-0001</span>
-            <h3 className="card-title">
-              {props.product.name}
-            </h3>
+            <h3 className="card-title">{props.product.name}</h3>
             <p className="card-price">{props.product.price}</p>
           </div>
         </article>
